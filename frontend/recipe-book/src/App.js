8def234c47f692ec3fb8fa929699027b0be602de
Dimitris <jim600v@gmail.com>
2023-09-22 22:44:31 +0300

    Install the FrontEnd and connecting with the Back End

---
+++ frontend/recipe-book/src/App.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import './App.css';
+import RecipeList from './components/RecipeList';
+import AddRecipe from './components/AddRecipe';
+
+function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/recipes');
+    } catch(error){
+      console.error('Error fetching recipes:', error);
+    }
+  };
+
+  useEffect(() => {
+      fetchRecipes();
+  }, []);
+
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1> Recipe Book </h1>
+        <AddRecipe refreshRecipes={fetchRecipes} />
+        <RecipeList recipes={recipes} refreshRecipes={fetchRecipes} />
+      </header>
+    </div>
+  );
+
+
+
+
+}
+
+export default App;
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recipes',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'recipebook.urls'
 
